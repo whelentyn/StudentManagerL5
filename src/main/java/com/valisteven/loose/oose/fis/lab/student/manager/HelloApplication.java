f03dfeddfc14264bf74565b556f81595d21cd83d
Steven Billich <stevenbillich17@gmail.com>
2022-03-21 11:08:51 +0200

    SM-2 Added the students Table and some dummy student list

--- src/main/java/com/valisteven/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ src/main/java/com/valisteven/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -1,7 +1,8 @@
-package com.valisteven.project_lab5;
+package com.valisteven.loose.oose.fis.lab.student.manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,13 +10,13 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Object root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("Student Manager");
+        primaryStage.setScene(new Scene((Parent) root, 600, 400));
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
